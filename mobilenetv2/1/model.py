56174221ffaf0b3224066ef7e3bc2c1b51d0f055
Heiru Wu <heiru.wu@instill.tech>
2023-11-13 15:41:26 +0800

    chore(ray): fix file extension

--- mobilenetv2/1/model.py
+++ mobilenetv2/1/model.py
@@ -148,7 +148,7 @@ def undeploy_model(model_name: str):
 
 
 if __name__ == "__main__":
-    func, model_config = entry()
+    func, model_config = entry("model.onnx")
 
     if func == "deploy":
         deploy_model(model_config=model_config)
