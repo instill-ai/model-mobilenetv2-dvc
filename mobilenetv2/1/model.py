e6dbacdcc82d43aabb18f2cf2ced0aac067ca0a5
Heiru Wu <heiru.wu@instill.tech>
2023-11-17 01:49:00 +0800

    chore(ray): add ray addr

--- mobilenetv2/1/model.py
+++ mobilenetv2/1/model.py
@@ -4,6 +4,7 @@ import ray
 import torch
 import requests
 
+from ray import serve
 import numpy as np
 import onnxruntime as ort
 from torchvision import transforms
@@ -12,7 +13,6 @@ from instill.helpers.const import DataType
 from instill.helpers.ray_io import serialize_byte_tensor, deserialize_bytes_tensor
 from instill.helpers.ray_config import (
     InstillRayModelConfig,
-    get_compose_ray_address,
     entry,
 )
 
@@ -25,10 +25,6 @@ from ray_pb2 import (
     ModelInferResponse,
     InferTensor,
 )
-
-ray.init(address=get_compose_ray_address(10001))
-# this import must come after `ray.init()`
-from ray import serve
 
 
 @serve.deployment()
@@ -149,7 +145,10 @@ def undeploy_model(model_name: str):
 if __name__ == "__main__":
     func, model_config = entry("model.onnx")
 
+    ray.init(address=model_config.ray_addr)
+
     if func == "deploy":
         deploy_model(model_config=model_config)
     elif func == "undeploy":
         undeploy_model(model_name=model_config.model_name)
+
